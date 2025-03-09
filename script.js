c3f943ad865ea02966ba50349dd0f1e59ecd66b8
Pablo <pr.pablo06@gmail.com>
2025-03-09 21:32:41 +0100

    Primer commit: Subida inicial del proyecto

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("verPersonajes").addEventListener("click", () => {
+    window.location.href = "Personajes.html";
+});
+
+document.getElementById("verTemporadas").addEventListener("click", () => {
+    window.location.href = "VerTemporadas.html";
+});
